8ef51be1912c149a0de5e6286b37d802a4533fa5
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-05-02 07:46:05 -0300

    Novidades do Spring 6: Problem Details

--- spring-problem-details/src/main/java/br/com/giulianabezerra/springproblemdetails/ControllerExceptionHandlers.java
+++ spring-problem-details/src/main/java/br/com/giulianabezerra/springproblemdetails/ControllerExceptionHandlers.java
@@ -4,6 +4,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerExceptionHandlers {
 
     @ExceptionHandler(ConflictException.class)
-    private ResponseEntity<ErrorResponse> handleConflictException(ConflictException e) {
+    private ResponseEntity<ProblemDetail> handleConflictException(ConflictException e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
 
-        return ResponseEntity.status(HttpStatus.valueOf(409)).body(new ErrorResponse(e.getMessage(), sw.toString()));
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.valueOf(409), sw.toString());
+        problemDetail.setTitle(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.valueOf(409)).body(problemDetail);
     }
 
 }
