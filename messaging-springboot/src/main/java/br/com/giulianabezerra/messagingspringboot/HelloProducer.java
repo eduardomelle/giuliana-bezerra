f3a3fb1a92ecfdb88684885907fbfcf965f30e2d
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-08-12 12:14:53 -0300

    Introdução à Mensageria com Spring e Kafka

---
+++ messaging-springboot/src/main/java/br/com/giulianabezerra/messagingspringboot/HelloProducer.java
@@ -0,0 +1,19 @@
+package br.com.giulianabezerra.messagingspringboot;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public HelloProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        this.kafkaTemplate.send("hello-topic", message);
+    }
+
+}
