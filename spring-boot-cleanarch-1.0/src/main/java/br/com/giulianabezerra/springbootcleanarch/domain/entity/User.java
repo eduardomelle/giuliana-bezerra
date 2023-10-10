35c4fb80c2879480777aa08fa08c23b09e3330b5
Eduardo Orlandi Melle <eduardomelle@gmail.com>
2023-10-10 20:30:00 -0300

    Criando um projeto Spring Boot com Arquitetura Limpa

--- spring-boot-cleanarch-1.0/src/main/java/br/com/giulianabezerra/springbootcleanarch/domain/entity/User.java
+++
@@ -1,5 +0,0 @@
-package br.com.giulianabezerra.springbootcleanarch.domain.entity;
-
-public record User(String username, String password, String email) {
-
-}
--- spring-boot-cleanarch-1.0/src/main/java/br/com/giulianabezerra/springbootcleanarch/infrastructure/persistence/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.giulianabezerra.springbootcleanarch.infrastructure.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-}
